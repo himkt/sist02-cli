af9151e661b0ec1f8e3c7568db5968367024231e
himkt <himkt@klis.tsukuba.ac.jp>
2015-06-05 13:30:25 +0900

    merge sist02-cli with sist02

--- lib/sist02/cli/cinii.rb
+++ lib/sist02/cli/cinii.rb
@@ -1,3 +1,4 @@
+require 'sist02'
 require 'open-uri'
 require 'json'
 require 'date'
@@ -8,57 +9,14 @@ module Sist02
     class CiNii < Thor
       desc "cinii_articles NAID", "get cinii articles bibliography"
       def cinii_articles(naid)
-        begin
-          html = open("http://ci.nii.ac.jp/naid/#{naid}.json").read
-          json = JSON.parser.new(html)
-          hash = json.parse["@graph"][0]
-          title =  hash["dc:title"][0]["@value"]
-          creator_raw = hash["dc:creator"]
-          creator = ''
-          creator_raw.each do |item|
-            creator += "#{item[0]["@value"]}, "
-          end
-          creator = creator.gsub(/\,\s$/, '')
-          publication_name = hash["prism:publicationName"][0]["@value"]
-          year = hash["dc:date"].match(/\d{4}/)
-          volume = hash["prism:volume"]
-          number = hash["prism:number"]
-          start_p = hash["prism:startingPage"]
-          end_p = hash["prism:endingPage"]
-          result = "#{creator}. #{title}. #{publication_name}. #{year}, vol. #{volume}, no. #{number}, p. #{start_p}-#{end_p}."
-        rescue => e
-          result = e
-        end
-        puts result
-        return result
+        puts Sist02::CiNii.article_ref(naid)
+        return Sist02::CiNii.article_ref(naid)
       end
 
       desc "cinii_books NCID", "get cinii books bibliography"
       def cinii_books(ncid)
-        begin
-          html = open("http://ci.nii.ac.jp/ncid/#{ncid}.json").read
-          json = JSON.parser.new(html)
-          hash = json.parse["@graph"][0]
-          title = hash["dc:title"][0]["@value"]
-          publisher = hash["dc:publisher"][0]
-          year = hash["dc:date"].match(/\d{4}/)
-          edition = hash["prism:edition"]
-          author_ary = Array.new
-          hash["foaf:maker"].each do |item|
-            author_ary << item["foaf:name"][0]["@value"].gsub(/(\s|　|,)+/, '')
-          end
-          author = author_ary.join(", ")
-
-          ris = open("http://ci.nii.ac.jp/ncid/#{ncid}.ris").read
-          pages = ris.match(/EP  - ([a-z]*, )?\d+p/).to_s.gsub(/EP  - /, '')
-          pages = "ページ数不明" if pages == ''
-          result = "#{author}. #{title}. #{edition}, #{publisher}, #{year}, #{pages}." unless edition == nil
-          result = "#{author}. #{title}. #{publisher}, #{year}, #{pages}." if edition == nil # for bibliographic information that doesn't have edition display
-        rescue => e
-          result = e
-        end
-        puts result
-        return result
+        puts Sist02::CiNii.book_ref(ncid)
+        return Sist02::CiNii.book_ref(ncid)
       end
     end
   end
--- lib/sist02/cli/version.rb
+++ lib/sist02/cli/version.rb
@@ -1,5 +1,5 @@
 module Sist02
   module Cli
-    VERSION = "0.1.2"
+    VERSION = "0.2.0"
   end
 end
